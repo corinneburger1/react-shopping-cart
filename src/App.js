1bff3c7279e83d0e0e5a4325ab17a56ec32cc6fa
Corinne Burger <corinneburger1@gmail.com>
2019-01-16 12:29:14 -0600

    closes #3

--- src/App.js
+++ src/App.js
@@ -2,27 +2,69 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class ProductInformation extends React.Component {
   render() {
+    const product = this.props.product;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="Product-information">
+        <div class="Product-title">{product.title}</div>
+        <div class="Product-price">${product.price}</div>
       </div>
     );
   }
 }
+
+class ProductCard extends React.Component {
+  render() {
+    const product = this.props.product;
+    return (
+      <div class="Product-card">
+        <img class="Product-image" src={require(`./static/products/${product.sku}_1.jpg`)}/>
+        <ProductInformation product={this.props.product}/>
+      </div>
+    );
+  }
+}
+
+class ProductTable extends Component {
+  render() {
+    const products = this.props.products;
+    const gallery = products ? products.map(product => (
+      <ProductCard product={product}/>
+    )) : (<div>No Data</div>);
+    console.log(gallery);
+    return(
+      <ul>{gallery}</ul>
+    )
+  }
+}
+
+class App extends Component {
+
+  constructor(props) 
+  {
+    super(props)
+    this.state = {
+      productList: null
+    }
+  }
+
+  componentDidMount() {
+    import("./products.json")
+    .then(json => this.setState({productList: json.default.products}))
+  }
+
+  render() {
+    let my_json = {}
+
+    return (
+      <div>
+        <ProductTable products={this.state.productList} />
+      </div>
+    );
+  }
+
+}
+
 
 export default App;
